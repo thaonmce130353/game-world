b3e0946facc1db5d398f31b4a4c96439f995307b
Thao Nguyen <nguyenminhthaobt1@gmail.com>
2024-03-25 23:53:44 +0700

    init

---
+++ js/script.js
@@ -0,0 +1,57 @@
+"use strict";
+var is_fullscreen = false;
+
+function open_fullscreen() {
+    let game = document.getElementById("game-area");
+    if (is_fullscreen) {
+        // Exit fullscreen
+        is_fullscreen = false;
+        if (is_mobile_device()) {
+            game.style.position = "absolute";
+            document.getElementById("mobile-back-button").style.display = "none";
+            document.getElementById("game-player").style.display = "none";
+        } else {
+            if (game.requestFullscreen) {
+                game.requestFullscreen();
+            } else if (game.mozRequestFullScreen) { /* Firefox */
+                game.mozRequestFullScreen();
+            } else if (game.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+                game.webkitRequestFullscreen();
+            } else if (game.msRequestFullscreen) { /* IE/Edge */
+                game.msRequestFullscreen();
+            }
+        }
+    } else {
+        // Enter fullscreen
+        is_fullscreen = true;
+        if (is_mobile_device()) {
+            document.getElementById("game-player").style.display = "block";
+            game.style.position = "fixed";
+            document.getElementById("mobile-back-button").style.display = "flex";
+        } else {
+            if (game.requestFullscreen) {
+                game.requestFullscreen();
+            } else if (game.mozRequestFullScreen) { /* Firefox */
+                game.mozRequestFullScreen();
+            } else if (game.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+                game.webkitRequestFullscreen();
+            } else if (game.msRequestFullscreen) { /* IE/Edge */
+                game.msRequestFullscreen();
+            }
+        }
+    }
+};
+
+function is_mobile_device() {
+    if (navigator.userAgent.match(/Android/i) ||
+        navigator.userAgent.match(/webOS/i) ||
+        navigator.userAgent.match(/iPhone/i) ||
+        navigator.userAgent.match(/iPad/i) ||
+        navigator.userAgent.match(/iPod/i) ||
+        navigator.userAgent.match(/BlackBerry/i) ||
+        navigator.userAgent.match(/Windows Phone/i)) {
+        return true;
+    } else {
+        return false;
+    }
+}
