fa4962ad15915e4d13badaafa95828ca2bda5934
Thao Nguyen <nguyenminhthaobt1@gmail.com>
2024-03-30 19:12:27 +0700

    add pagination

--- js/category.js
+++ js/category.js
@@ -1,14 +1,15 @@
-var games = [];
+let categoryGames = [];
+var gamesPerPage = 18;
+
 const urlParams = new URLSearchParams(window.location.search);
 const category = urlParams.get('category');
+let currentPage = urlParams.get('page') ?? 1;
 document.getElementById("category-title").textContent = mapCategoryTitle(category?.toLowerCase());
 fetch('data.json')
     .then(response => response.json()) // Parse the data to a JavaScript object
     .then(response => {
-        games = response.games;
-        //Load the new games
-        const categoryContainer = document.querySelector('.games-list-container');
-        const categoryGames = games?.filter(game => {
+        const games = response.games;
+        categoryGames = games?.filter(game => {
             const categories = game.categories?.split(',')?.map(category => category.toLowerCase());
             if (category === 'New' || category === 'Trending') {
                 return true;
@@ -21,11 +22,11 @@ fetch('data.json')
             }
             return b.order - a.order;
         });
-
         if (categoryGames.length === 0) {
             document.getElementById("no-games").style.display = "block";
         } else {
-            appendItems(categoryGames, categoryContainer);
+            displayGames();
+            displayPagination();
         }
     }).catch(error => console.error('Error:', error));
 
@@ -60,5 +61,51 @@ function mapCategoryTitle(gameUrl) {
         case 'trending':
             return 'Trending Games';
     }
+}
+
+function displayGames() {
+    var start = (currentPage - 1) * gamesPerPage;
+    var end = start + gamesPerPage;
+    var gamesToDisplay = categoryGames.slice(start, end);
+
+    const categoryContainer = document.querySelector('.games-list-container');
+
+    $('.games-list-container').empty();
+
+    appendItems(gamesToDisplay, categoryContainer);
+}
+
+function displayPagination() {
+    var totalPages = Math.ceil(categoryGames.length / gamesPerPage);
+
+    var pagination = $('#pagination');
+    pagination.empty();
+
+    $('<li>').addClass('page-item').append($('<a>').addClass('page-link').attr('href', `#`).html('<i class="fas fa-chevron-left"></i>').click(function(e) {
+        e.preventDefault();
+        if (currentPage > 1) {
+            currentPage--;
+            displayGames();
+            displayPagination();
+        }
+    })).appendTo(pagination);
+
+    for (var i = 1; i <= totalPages; i++) {
+        $('<li>').addClass(i === currentPage ? 'page-item active' : 'page-item').append($('<a>').addClass('page-link').attr('href', `#`).text(i).click(function(e) {
+            e.preventDefault();
+            currentPage = parseInt($(this).text());
+            displayGames();
+            displayPagination();
+        })).appendTo(pagination);
+    }
+
+    $('<li>').addClass('page-item').append($('<a>').addClass('page-link').attr('href', '#').html('<i class="fas fa-chevron-right"></i>').click(function(e) {
+        e.preventDefault();
+        if (currentPage < totalPages) {
+            currentPage++;
+            displayGames();
+            displayPagination();
+        }
+    })).appendTo(pagination);
 }
 
